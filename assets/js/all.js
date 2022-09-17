5e69b9dfd2ee7de9f1d12953ee3d3a6bdb04a7aa
barret0720 <jianhong.liou@gmail.com>
2022-09-18 01:46:28 +0800

    Update 2022-09-17T17:46:24.984Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,19 +5,30 @@ $(function () {
 }); // add class navbarDark on navbar scroll
 
 var header = document.querySelector(".navbar");
+var link = document.querySelector(".nav-link");
 var page = location.pathname;
 console.log(page);
 
-window.onscroll = function () {
-  var top = window.scrollY;
+if (page == '/qa.html' || page == '/alarm.html') {
+  header.classList.remove("fixed-top", "mt-4", "mt-md-10");
+  header.classList.add("bg-secondary");
+  link.classList.add("text-primary");
+  link.classList.remove("text-lg-gray1", "text-white");
+  console.log("YES");
+} else {
+  window.onscroll = function () {
+    var top = window.scrollY;
 
-  if (top >= 86) {
-    // header.classList.add("bg-secondary");
-    // header.classList.add("sticky-top");
-    header.classList.remove("fixed-top");
-  } else {
-    header.classList.add("fixed-top"); // header.classList.remove("bg-secondary");
-    // header.classList.remove("sticky-top");
-  }
-};
+    if (top >= 86) {
+      // header.classList.add("bg-secondary");
+      // header.classList.add("sticky-top");
+      header.classList.remove("fixed-top");
+    } else {
+      header.classList.add("fixed-top"); // header.classList.remove("bg-secondary");
+      // header.classList.remove("sticky-top");
+    }
+  };
+}
+
+;
 //# sourceMappingURL=all.js.map
