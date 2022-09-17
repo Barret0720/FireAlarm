eb3bc84697a7bb3078e1ecc3ef4a0e20bb3a47ed
barret0720 <jianhong.liou@gmail.com>
2022-09-18 00:14:38 +0800

    Update 2022-09-17T16:14:35.879Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,23 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
-});
+  console.log("一個住警器，守護全家人");
+}); // add class navbarDark on navbar scroll
+
+var header = document.querySelector(".navbar");
+var page = location.pathname;
+console.log(page);
+
+window.onscroll = function () {
+  var top = window.scrollY;
+
+  if (top >= 86) {
+    // header.classList.add("bg-secondary");
+    // header.classList.add("sticky-top");
+    header.classList.remove("fixed-top");
+  } else {
+    header.classList.add("fixed-top"); // header.classList.remove("bg-secondary");
+    // header.classList.remove("sticky-top");
+  }
+};
 //# sourceMappingURL=all.js.map
