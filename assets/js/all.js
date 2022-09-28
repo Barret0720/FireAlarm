43673d45563744d2f5dca86e5adf070ebb8bbe50
Barret <jianhong.liou@gmail.com>
2022-09-29 00:24:12 +0800

    Update 2022-09-28T16:24:09.860Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -35,29 +35,29 @@ if (host == GitHub) {
       }
     };
   }
-}
-
-;
-
-if (page == '/qa.html' || page == '/alarm.html') {
-  header.classList.remove("fixed-top", "mt-4", "mt-md-10");
-  header.classList.add("bg-secondary");
-  link.classList.add("text-primary");
-  link.classList.remove("text-lg-gray1", "text-white");
-  console.log("YES");
 } else {
-  window.onscroll = function () {
-    var top = window.scrollY;
+  if (page == '/qa.html' || page == '/alarm.html') {
+    header.classList.remove("fixed-top", "mt-4", "mt-md-10");
+    header.classList.add("bg-secondary");
+    link.classList.add("text-primary");
+    link.classList.remove("text-lg-gray1", "text-white");
+    console.log("YES");
+  } else {
+    window.onscroll = function () {
+      var top = window.scrollY;
 
-    if (top >= 86) {
-      // header.classList.add("bg-secondary");
-      // header.classList.add("sticky-top");
-      header.classList.remove("fixed-top");
-    } else {
-      header.classList.add("fixed-top"); // header.classList.remove("bg-secondary");
-      // header.classList.remove("sticky-top");
-    }
-  };
+      if (top >= 86) {
+        // header.classList.add("bg-secondary");
+        // header.classList.add("sticky-top");
+        header.classList.remove("fixed-top");
+      } else {
+        header.classList.add("fixed-top"); // header.classList.remove("bg-secondary");
+        // header.classList.remove("sticky-top");
+      }
+    };
+  }
+
+  ;
 }
 
 ;
